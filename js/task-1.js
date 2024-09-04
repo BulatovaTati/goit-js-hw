7d7a623248cb6d1f14942b3f00e299beef7e63af
BulatovaTati <kohodatat@gmail.com>
2024-09-04 17:56:05 +0300

    task2

--- js/task-1.js
+++ js/task-1.js
@@ -3,14 +3,12 @@ const categories = document.querySelectorAll('#categories .item');
 console.log('Number of categories: ', categories.length); // Number of categories:  3
 
 categories.forEach(category => {
-  const categoryTitle = category.querySelector('h2').textContent;
-
   const categoryElementsCount = category.querySelectorAll('ul li');
 
   for (let i = 0; i < categoryElementsCount.length; i++) {
     categoryElementsCount[i].classList.add('option-item');
   }
 
-  console.log(`Category: ${categoryTitle}`); // Category: ;
-  console.log(`Elements: ${categoryElementsCount.length}`); // Elements: ;
+  console.log('Category:', category.firstElementChild.textContent);
+  console.log('Elements:', category.lastElementChild.children.length);
 });
