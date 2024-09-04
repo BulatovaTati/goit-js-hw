964b91dbca9c2d2e74928fd8f515f3c09bd71b91
BulatovaTati <kohodatat@gmail.com>
2024-09-04 18:48:14 +0300

    add task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', checkValid);
+
+function checkValid(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('All form fields must be filled in');
+  }
+
+  console.log(`email: ${email.value}, password: ${password.value}`);
+  event.currentTarget.reset();
+}
