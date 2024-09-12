4fdbcdfcf6197fb84cfdc16d27eedd4bd947e93f
BulatovaTati <kohodatat@gmail.com>
2024-09-12 21:36:27 +0300

    add form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,22 +1,44 @@
 const form = document.querySelector('.feedback-form');
+
 const localStorageKey = 'feedback-form-state';
 
-form.addEventListener('input', evt => {
-  localStorage.setItem(localStorageKey, evt.target.value);
-});
+try {
+  const { email, message } =
+    JSON.parse(localStorage.getItem(localStorageKey)) ?? '';
+  form.elements.email.value = email ?? '';
+  form.elements.message.value = message ?? '';
+} catch (error) {
+  console.log(error.name);
+  console.log(error.message);
+}
 
 const formData = {
   email: '',
   message: '',
 };
 
-form.addEventListener('input', evt => {
-  localStorage.setItem(localStorageKey, evt.target.value);
-});
+form.addEventListener('input', onFormData);
+form.addEventListener('submit', onSubmitForm);
 
-form.addEventListener('submit', evt => {
-  evt.preventDefault();
-  console.log(evt.target.elements.message.value);
-  localStorage.removeItem(localStorageKey);
+function onFormData(e) {
+  const { name, value } = e.target;
+
+  formData[name] = value;
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+}
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  const data = localStorage.getItem(localStorageKey);
+
+  if (!data) {
+    return alert('Oops, fiels is empty');
+  }
+
+  const parsedData = JSON.parse(data);
+  console.log('parsedData: ', parsedData);
+
   form.reset();
-});
+  localStorage.removeItem(localStorageKey);
+}
